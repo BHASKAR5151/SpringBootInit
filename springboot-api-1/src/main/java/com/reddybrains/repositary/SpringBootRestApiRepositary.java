a070ad42926c8c16f4f22608d97d1cf388826b7f
BHASKAR <dongalabhaskarreddy@gmail.com>
2019-06-05 13:38:40 +0530

    files

---
+++ springboot-api-1/src/main/java/com/reddybrains/repositary/SpringBootRestApiRepositary.java
@@ -0,0 +1,9 @@
+package com.reddybrains.repositary;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.reddybrains.controller.SpringBootRestApiForm;
+
+public interface SpringBootRestApiRepositary extends CrudRepository<SpringBootRestApiForm ,Long>{
+
+}
